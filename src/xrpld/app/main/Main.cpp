7837482f804a3df1a8f4c6223b63e961a68f47bb
Wishaal Kanhai <wishaal.kanhai@simplifynow.nl>
2025-06-07 11:14:11 +0200

    change validation quorum

--- src/xrpld/app/main/Main.cpp
+++ src/xrpld/app/main/Main.cpp
@@ -773,7 +773,7 @@ run(int argc, char** argv)
     {
         try
         {
-            config->VALIDATION_QUORUM = vm["quorum"].as<std::size_t>();
+            config->VALIDATION_QUORUM = 2;  // 2 validators to consider a ledger validated...
             if (config->VALIDATION_QUORUM == std::size_t{})
             {
                 throw std::domain_error("0");
